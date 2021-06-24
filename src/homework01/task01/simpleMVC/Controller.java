75cfb5aee7006f1c6450bec54c441d9e28660908
MS <jfiekd@gmail.com>
2021-06-24 14:33:34 +0300

    homework01 update

---
+++ src/homework01/task01/simpleMVC/Controller.java
@@ -0,0 +1,52 @@
+package homework01.task01.simpleMVC;
+
+/**
+ *
+ * created by MS on 24.06.2021
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Controller {
+    //Constructor
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    //Work method
+    public void processUser(){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            for (int i = 0; i < 2; i++) {
+                model.constructString(getInputFromUser(reader));
+                if (i==0)
+                    model.constructString(" ");
+            }
+            reader.close();
+        }
+        catch (IOException ignored){}
+
+        view.printMessage(model.getValue());
+    }
+
+    //Utility methods
+    public String getInputFromUser(BufferedReader reader) throws IOException{
+        view.printMessage(View.INPUT_CORRECT_LINE);
+        String input = reader.readLine();
+        while (!isAcceptable(input)) {
+            view.printMessage(View.INCORRECT_LINE + " " + View.INPUT_CORRECT_LINE);
+            input = reader.readLine();
+        }
+        return input;
+    }
+
+    public boolean isAcceptable(String str){
+        return str.equals("Hello") && model.getValue().isEmpty() || str.equals("world!") && !model.getValue().isEmpty();
+    }
+}
