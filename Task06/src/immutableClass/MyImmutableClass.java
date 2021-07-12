50232c12c3d844f44674b94b605c778541c7a737
Mykhailo Shekera <jfiekd@gmail.com>
2021-07-12 17:51:38 +0300

    upd

--- Task06/src/immutableClass/MyImmutableClass.java
+++ Task06/src/immutableClass/MyImmutableClass.java
@@ -1,5 +1,22 @@
 package immutableClass;
 
-public class MyImmutableClass {
+import java.util.ArrayList;
+import java.util.List;
 
+public final class MyImmutableClass {
+    private final int id;
+    private final List<String> list;
+
+    public MyImmutableClass(int id, List<String> list) {
+        this.id = id;
+        this.list = new ArrayList<>(list);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<String> getList() {
+        return new ArrayList<>(list);
+    }
 }
