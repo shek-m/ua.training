a99889d85ac51899faf87844487f1dd6abb49c0a
Mykhailo Shekera <jfiekd@gmail.com>
2021-07-12 16:26:20 +0300

    adding Employee.java to Task06: training of equals(Object obj) and hashCode()

---
+++ Task06/src/equalsAndHashcodeTraining/Employee.java
@@ -0,0 +1,62 @@
+package equalsAndHashcodeTraining;
+
+public class Employee {
+    private int age;
+    private String name;
+    private String surname;
+
+    public Employee () {}
+
+    public Employee (int age, String name, String surname) {
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Employee) {
+            Employee temp = (Employee) obj;
+            return this.age == temp.age && this.name.equals(temp.name)
+                    && this.surname.equals(temp.surname);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return (31 * age + (name == null ? 0 : name.hashCode()))
+                * 31 + (surname == null ? 0 : surname.hashCode());
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
