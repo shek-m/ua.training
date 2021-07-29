380b5019bf5eaa57e79d5203a255512646f6ffe4
Mykhailo Shekera <jfiekd@gmail.com>
2021-07-30 01:41:50 +0300

    margin added to lang changing buttons complicated method viewHomePage is replaced with simple

--- Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
+++ Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
@@ -12,19 +12,24 @@ import java.util.Locale;
 public class PageController {
 
     @GetMapping("/")
-    public String viewHomePage(Model model, HttpServletRequest request){
-        model.addAttribute("page title", "Tax Service Home Page");
-        Locale currentLocale = request.getLocale();
-        String countryCode = currentLocale.getCountry();
-        String countryName = currentLocale.getDisplayCountry();
-
-        String languageCode = currentLocale.getLanguage();
-        String languageName = currentLocale.getDisplayLanguage();
-
-        log.info("{}", countryCode + " : " + countryName);
-        log.info("{}", languageCode + " : " + languageName);
-
+    public String viewHomePage(){
         return "index";
     }
+
+//    @GetMapping("/")
+//    public String viewHomePage(Model model, HttpServletRequest request){
+//        model.addAttribute("page title", "Tax Service Home Page");
+//        Locale currentLocale = request.getLocale();
+//        String countryCode = currentLocale.getCountry();
+//        String countryName = currentLocale.getDisplayCountry();
+//
+//        String languageCode = currentLocale.getLanguage();
+//        String languageName = currentLocale.getDisplayLanguage();
+//
+//        log.info("{}", countryCode + " : " + countryName);
+//        log.info("{}", languageCode + " : " + languageName);
+//
+//        return "index";
+//    }
 
 }
