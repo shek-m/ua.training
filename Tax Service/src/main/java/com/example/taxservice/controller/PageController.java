376ed8537ef3def2a348253687dbcd5daedd1d1e
Mykhailo Shekera <jfiekd@gmail.com>
2021-07-30 10:11:55 +0300

    minor update

--- Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
+++ Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
@@ -2,34 +2,17 @@ package com.example.taxservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Locale;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 public class PageController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String viewHomePage(){
         return "index";
     }
 
-//    @GetMapping("/")
-//    public String viewHomePage(Model model, HttpServletRequest request){
-//        model.addAttribute("page title", "Tax Service Home Page");
-//        Locale currentLocale = request.getLocale();
-//        String countryCode = currentLocale.getCountry();
-//        String countryName = currentLocale.getDisplayCountry();
-//
-//        String languageCode = currentLocale.getLanguage();
-//        String languageName = currentLocale.getDisplayLanguage();
-//
-//        log.info("{}", countryCode + " : " + countryName);
-//        log.info("{}", languageCode + " : " + languageName);
-//
-//        return "index";
-//    }
 
 }
