3d89209574ee9274e0dc43408aec8482835192b6
Mykhailo Shekera <jfiekd@gmail.com>
2021-08-01 12:19:20 +0300

    authorization data on the main page

--- Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
+++ Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
@@ -1,18 +1,23 @@
 package com.example.taxservice.controller;
 
+import com.example.taxservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
+    @Autowired
+    private UserService userService;
 
-    @RequestMapping("/")
-    public String viewHomePage(){
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("user", userService.getActiveUser());
         return "index";
     }
 
     @RequestMapping("/register")
     public String registrationPage() { return "reg_form";}
-
-
 }
--- Tax Service/src/main/java/com/example/taxservice/service/UserService.java
+++ Tax Service/src/main/java/com/example/taxservice/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.taxservice.service;
 
 import com.example.taxservice.entity.User;
 import com.example.taxservice.repository.UserRepository;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +16,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    @Getter
+    private User activeUser;
 
     @Autowired
-    public UserService(UserRepository repository){
+    public UserService(UserRepository repository) {
         this.userRepository = repository;
     }
 
     @Override
     public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User \"" + username + "\" is not registered"));
-
+        this.activeUser = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User \"" + username + "\" is not registered"));
+        return activeUser;
     }
 
-    public void saveNewUser (User user){
+    public void saveNewUser(User user) {
         try {
             userRepository.save(user);
-        } catch (Exception ex){
+        } catch (Exception ex) {
             log.info("{Почтовый адрес уже существует}");
         }
-
     }
 }
