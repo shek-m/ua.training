a267ffd87800e5102e44998074297d36c35e6e33
Mykhailo Shekera <jfiekd@gmail.com>
2021-08-03 11:43:40 +0300

    checkpoint_registration

--- Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
+++ Tax Service/src/main/java/com/example/taxservice/controller/PageController.java
@@ -33,8 +33,8 @@ public class PageController {
     }
 
 
-    @RequestMapping("/register")
-    public String registrationPage() { return "reg_form";}
+    @GetMapping("/registration")
+    public String registrationPage() { return "registration";}
 
     @GetMapping("/")
     public String welcomePage() { return "welcome";}
